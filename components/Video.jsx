2aa0a7f2f3eaa92005aacc1f8bfb8ef5bd315ba6
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2024-01-18 18:52:45 -0300

    video

--- components/Video.jsx
+++ components/Video.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { PiYoutubeLogoLight } from "react-icons/pi";
 
-
 const video = () => {
   return (
     <div id="services" className="container mx-auto text-black p-5 ">
@@ -9,19 +8,15 @@ const video = () => {
         <div className="grid text-center text-2xl text-black  p-4 ">
           <h2 className="p-4">Os planos na pratica!</h2>
           <div className="flex items-center justify-center ">
-            <PiYoutubeLogoLight />
-            <p className="p-2">Video demonstrativo</p>
+          < PiYoutubeLogoLight />
+          <p className="p-2">Video demonstrativo</p>
           </div>
         </div>
-        <div className="">
+        <div className="p-4">
           <video
-          controls
-            src="/assets/video/video-1.mp4"
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-            className="rounded-2xl"
+            controls
+            src={"/assets/videos/video-1.mp4"}
+            style={{ width: "864px", height: "480px" }}
           />
         </div>
       </div>
