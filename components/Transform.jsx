f1b59ee17411f16929adab58790047fd1d52c41b
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2024-01-26 14:37:00 -0300

    responsivo para desktop

--- components/Transform.jsx
+++ components/Transform.jsx
@@ -7,30 +7,38 @@ import Link from "next/link";
 const Transform = () => {
   return (
     <section id="transform" className="container mx-auto">
-      <div className="grid p-5 mt-20">
-        <h2>Transforme sua presença nas redes sociais</h2>
-
-        <p className="mt-4"> 
-          aumente seu alcance, engajamento e retorno sobre o investimento com
-          nossos planos personalizados de midia social.
-        </p>
-        <div className="grid  justify-items-center p-5">
-        <ul>
-          <li className="flex items-center p-4 hover:text-purple-600">
-            <Link className="p-2" href="/contact">Saber mais</Link>
-            <MdArrowRightAlt />
-          </li>
-         
-        </ul>
-
-        <Image
-          src={ProdutoImg1}
-          width={300}
-          height={200}
-          alt="imagem"
-          className="p-4"
-        />
+      <div className="grid md:grid-cols-3 p-5 mt-20">
+      
+        <div>
+          <h2>Transforme sua presença nas redes sociais</h2>
+          <p className="mt-4">
+            aumente seu alcance, engajamento e retorno sobre o investimento com
+            nossos planos personalizados de midia social.
+          </p>
+          <ul>
+            <li className="mt-4">
+              <Link href="/contact">
+                <button className="p-2">Começar</button>
+              </Link>
+            </li>
+          </ul>
         </div>
+       
+          <div className="grid justify-center">
+            <Image
+              src={ProdutoImg1}
+              width={300}
+              height={200}
+              alt="imagem"
+              className="p-4"
+            />
+          </div>
+
+          <div className=" ">
+            <h2>Ouça a voz das redes sociais</h2>
+            <p>Não deixe seus clientes sem respostas nunca mais!</p>
+          </div>
+        
       </div>
     </section>
   );
