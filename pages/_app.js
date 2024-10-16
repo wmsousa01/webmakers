2b0f30811b0b40a1f21b3699cde3c1d1d322671f
willian.m.sousa@gmail.com <willian.m.sousa@gmail.com>
2024-10-16 09:57:56 -0300

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,53 @@
-import React, { useEffect } from "react";
+import { useEffect } from 'react';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import ChatGPTChat from "../components/gpt/ChatGptChat"; // Importe o componente do chat
-import "../styles/globals.css";
-import { ToastContainer, toast } from "react-toastify";
+import '../styles/globals.css';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    // Tawk.to Script
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/66acb00032dca6db2cb9249c/1i498v0jd';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-    })();
-  }, []);
-
-  useEffect(() => {
+    // Toast notification
     toast(
-      <div style={{ fontSize: "18px", fontWeight: "bold", fontFamily: "Montserrat, sans-serif", textAlign: "center", color: "#333" }}>
-        Aproveite <span style={{ color: "#39B6EB" }}>20% de Desconto</span> no Seu Novo Site!
+      <div style={{ fontSize: "20px", fontWeight: "bold", fontFamily: "Montserrat, sans-serif", textAlign: "center", color: "#000" }}>
+        Aproveite <span style={{ color: "#38b6ff" }}>20%</span> de Desconto no Seu Novo Site!
       </div>,
       {
-        position: "top-center",
-        autoClose: 15000,  // Fecha automaticamente em 15 segundos
+        position: "top-right",
+        autoClose: 15000, // Fechamento automático após 15 segundos
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         style: {
-          background: "#FFF", // Fundo branco
-          borderRadius: "12px",  // Bordas arredondadas
-          boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",  // Sombra suave
-          border: "1px solid #39B6EB",  // Borda azul suave
-          padding: "15px",  // Padding para uma sensação mais "arejada"
+          background: "#FFF", // Fundo claro
         },
-        progressStyle: {
-          background: "#39B6EB",  // Cor do progresso para combinar com a marca
-        }
       }
     );
   }, []);
 
   return (
     <div>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `,
+        }}
+      />
+
+      {/* ToastContainer com ajustes */}
       <ToastContainer 
         position="top-right"
         autoClose={15000}
@@ -60,9 +60,9 @@ function MyApp({ Component, pageProps }) {
         pauseOnHover
         theme="light"  // Tema mais leve e claro
       />
+
       <Navbar />
       <Component {...pageProps} />
-      <ChatGPTChat /> {/* Chat exibido em todas as telas */}
       <Footer />
     </div>
   );
